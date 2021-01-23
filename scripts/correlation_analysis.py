a133188c7f2273e3018f7f7de154145325866747
stanlee1986 <51210436+stanlee1986@users.noreply.github.com>
2021-01-22 22:52:09 -0500

    Update correlation_analysis.py
    fixed NaNs in correlation by dropping first two rows of shift(1)*2, deleted call to backtest_long that is unused

--- scripts/correlation_analysis.py
+++ scripts/correlation_analysis.py
@@ -18,15 +18,13 @@ dates = []
 index = 0
 
 for name, group in df.groupby('date'):
-    backtest_long(group, -60, -30, -100)
-    exit()
     index = index + 1
     if len(group[group['NOPE_allVolume'].isna()]):
         continue
     group['spy_change'] = group['active_underlying_price'].shift(1)
     group['spy_change'] = 100*(group['active_underlying_price']-group['spy_change'])/group['spy_change']
     group['prior_spy_change'] = group['spy_change'].shift(1)
-    group = group[:-1]
+    group = group[2:-1]
     corr.append(stats.pearsonr(group['spy_change'], group['NOPE_busVolume'])[0])
     corr_all.append(stats.spearmanr(group['shifted_spy_change'], group['NOPE_busVolume'])[0])
     dates.append(index)
